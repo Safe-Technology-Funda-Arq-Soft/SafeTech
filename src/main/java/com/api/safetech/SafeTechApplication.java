44ca2368acdd45732e9148043b7185012718d519
Jose Tarazona <joset2804@hotmail.com>
2022-04-16 20:18:31 -0500

    Create Safe Techology API

---
+++ src/main/java/com/api/safetech/SafeTechApplication.java
@@ -0,0 +1,13 @@
+package com.api.safetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeTechApplication.class, args);
+    }
+
+}
